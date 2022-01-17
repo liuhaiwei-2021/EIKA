c4466908cbffe358a0be075e33b95e513152e02f
Haiwei Liu <haiweiliu@Haiweis-MacBook-Air.local>
2022-01-17 15:24:42 +0100

    resolve input default background color

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./Header";
 import Welcome from "./Welcome";
 import ShoppingList from "./ShoppingList";
-
 import { Routes, Route } from "react-router-dom";
 
 
